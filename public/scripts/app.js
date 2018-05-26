e38faaea316decc6c7283f89d918af3326a49a30
Angelo Cappelletti <angelocapp97@gmail.com>
2018-05-26 15:36:08 +0200

    Initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,10 @@
+
+var template = React.createElement(
+    "h1",
+    { id: 'title' },
+    "Hello React!"
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
