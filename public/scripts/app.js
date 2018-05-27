9dfab9d85cb27219521e3d00086d2f14adae7ced
Angelo Cappelletti <angelocapp97@gmail.com>
2018-05-27 15:38:21 +0200

    The first React app code example

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,55 @@
-"use strict";
+'use strict';
+
+var app = {
+    title: 'Indecision App',
+    subtitle: 'My first React app',
+    options: ['Option one', 'Option two', 'Option three'],
+    getOptions: function getOptions() {
+        var _this = this;
+
+        return app.options && app.options.length > 0 ? React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'p',
+                null,
+                'There are some options:'
+            ),
+            React.createElement(
+                'ol',
+                null,
+                this.options.map(function (option) {
+                    return React.createElement(
+                        'li',
+                        { key: _this.options.indexOf(option) },
+                        option
+                    );
+                })
+            )
+        ) : React.createElement(
+            'p',
+            { className: 'no-options' },
+            'There are no options to show.'
+        );
+    }
+};
 
 var template = React.createElement(
-  "h1",
-  { id: "title" },
-  "Hello React"
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title ? app.title : 'Anonymous'
+    ),
+    app.subtitle && React.createElement(
+        'h3',
+        null,
+        app.subtitle
+    ),
+    app.getOptions()
 );
 
-var appRoot = document.getElementById('app');
+var appRoot = document.getElementById('app-root');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,30 @@
 
-var template = <h1 id="title">Hello React</h1>
+const app = {
+    title: 'Indecision App',
+    subtitle: 'My first React app',
+    options: ['Option one', 'Option two', 'Option three'],
+    getOptions() {
+        return (
+            (app.options && app.options.length > 0) ? (
+                <div>
+                    <p>There are some options:</p>
+                    <ol>
+                        {this.options.map((option) => <li key={this.options.indexOf(option)}>{option}</li>)}
+                    </ol>
+                </div>
+            ) : <p className="no-options">There are no options to show.</p>
+        )
+    }
+};
 
-var appRoot = document.getElementById('app')
+const template = (
+    <div>
+        <h1>{app.title ? app.title : 'Anonymous'}</h1>
+        {app.subtitle && <h3>{app.subtitle}</h3>}
+        {app.getOptions()}
+    </div>
+)
+
+const appRoot = document.getElementById('app-root')
 
 ReactDOM.render(template, appRoot)
