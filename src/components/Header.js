503321654d4b2201e441e616509682e3b870d4b2
Angelo Cappelletti <angelocapp97@gmail.com>
2018-06-01 16:40:53 +0200

    Refactored stateless functional components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
-export const Header = () => {
-    return (
-        <div>
-            <h1>Indecision App</h1>
-        </div>
-    )
-}
+export const Header = () => (
+    <div>
+        <h1>Indecision App</h1>
+    </div>
+)
